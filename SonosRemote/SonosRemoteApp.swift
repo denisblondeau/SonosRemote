b5520b72dfb60244e345fc529c289d34cff136a4
Denis Blondeau <dbappdev@outlook.com>
2024-03-04 11:35:25 -0500

    Initial Commit

---
+++ SonosRemote/SonosRemoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  SonosRemoteApp.swift
+//  SonosRemote
+//
+//  Created by Denis Blondeau on 2024-03-04.
+//
+
+import SwiftUI
+
+@main
+struct SonosRemoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
